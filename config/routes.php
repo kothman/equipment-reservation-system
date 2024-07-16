a7cf18eb8e5e5ffc07108c4f937102d7e8873dbb
Morgan Kothman <abkothman@gmail.com>
2024-07-16 02:56:20 +0000

    Update routes config to use grouping

--- config/routes.php
+++ config/routes.php
@@ -15,5 +15,11 @@ $router = new Router();
 $router->match('GET', '/', Controller::class, 'index', 'index.html');
 $router->match('GET', '/login', Controller::class, 'index', 'index.html');
 $router->match('GET', '/logout', Controller::class, 'index', 'index.html');
+$router->group('/users', function() use ($router) {
+    $router->match('GET', '/', UserController::class, 'index', 'users/index.html');
+    $router->match('GET', '/create/', UserController::class, 'showCreate', 'users/create.html');
+    $router->match('POST', '/create/', UserController::class, 'handleCreate', 'users/show.html');
+    $router->match('GET', '/([\w]+)(/)?', UserController::class, 'showUser', 'users/show.html');
+});
 
 return $router;
